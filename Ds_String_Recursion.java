1f9d45243a83d54579dd3fb80bc92a7bfbee3800
priyaganesh1504 <42537456+priyaganesh1504@users.noreply.github.com>
2019-08-18 15:11:08 +0530

    Create Ds_String_Recursion.java
    Program to reverse a String Using Recursion

---
+++ Ds_String_Recursion.java
@@ -0,0 +1,21 @@
+package datastructures.java.com;
+
+import java.util.Scanner;
+
+public class Ds_String_Recursion {
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		String s1=scan.nextLine();
+		scan.close();
+		String rev=reverseString(s1);
+		System.out.println("The reversed String is: "+rev);
+	}
+	public static String reverseString(String s1) {
+		if(s1.isEmpty()) 
+			return s1;
+		
+		
+		return reverseString(s1.substring(1))+s1.charAt(0);
+	
+	}
+}
